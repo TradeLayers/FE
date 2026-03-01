bc4f678a96ba3618b28db3c3bb76220bba16d1ab
Titas Aleškevičius <titaslr38@gmail.com>
2026-03-01 23:19:24 +0200

    STC-13: environmental variables (#13)
    * STC-13: updated axiosConfig.ts baseURL
    
    * STC-13: Added .env.example file
    
    * STC-13: Updated .gitignore with .env files

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({
-    baseURL: 'tempUrl',
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
