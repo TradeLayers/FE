72b8b462c20d8e464a3860734496b9edb7f0cd0d
Titas <titaslr38@gmail.com>
2026-03-17 22:16:58 +0200

    Button displays Log Out when user is logged in

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,32 +1,55 @@
-import { Outlet, Link as RouterLink } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { Box, Button, Typography } from '@mui/material';
+import { signOut } from 'firebase/auth';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { auth } from '@configs/firebase';
+import { UserType } from '@models/userTypes';
+import { type RootState } from '@store/store';
+import { resetUserInfo } from '@store/userSlice';
 
 const MainPage: React.FC = () => {
-    return (
-        <Box 
-            sx={{ p: 3 }}>
-            <Box 
-                sx={{ display: 'flex', 
-                    justifyContent: 'space-between', 
-                    alignItems: 'center', 
-                    mb: 3 }}>
-                <Typography 
-                    variant="h4" 
-                    component="h1">
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const userType = useSelector((state: RootState) => state.userSliceName.userType);
+    const isLoggedIn = userType !== UserType.Guest;
 
+    const handleAuthButtonClick = async () => {
+        if (!isLoggedIn) {
+            navigate('/login');
+            return;
+        }
+
+        await signOut(auth);
+        dispatch(resetUserInfo());
+        navigate('/');
+    };
+
+    return (
+        <Box sx={{ p: 3 }}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    mb: 3,
+                }}
+            >
+                <Typography variant="h4" component="h1">
                     Stock Tracker
                 </Typography>
-                <Button 
-                    variant="outlined" 
+                <Button
+                    variant="outlined"
                     size="large"
-                    sx={{ 
-                        px: 4, 
-                        py: 1.5, 
-                        fontSize: '1.1rem' }}
-                    component={RouterLink} 
-                    to="/login">
-
-                    Login
+                    sx={{
+                        px: 4,
+                        py: 1.5,
+                        fontSize: '1.1rem',
+                        backgroundColor: '#16171dff',
+                    }}
+                    onClick={handleAuthButtonClick}
+                >
+                    {isLoggedIn ? 'Log Out' : 'Log In'}
                 </Button>
             </Box>
             <Outlet />
