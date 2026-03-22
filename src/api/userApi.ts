8a1d2f29f72f2c8666ff28eac8ba7ede5736c74f
MatasBrazauskas <matasbrazauskas123456@gmail.com>
2026-03-22 16:13:13 +0200

    Changed Gemini model

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,7 +1,7 @@
-import { protectedApi } from "./axiosConfig";
-import { type User } from "@models/userTypes";
+import { protectedApi } from './axiosConfig';
+import { type User } from '@models/userTypes';
 
 export const createOrFetchUser = async (): Promise<User> => {
     const response = await protectedApi.post<User>('/user');
-    return response.data
-}
+    return response.data;
+};
