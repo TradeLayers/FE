1f47ee364df665e4969816362bbbc6c6e1c37704
MatasBrazauskas <matasbrazauskas123456@gmail.com>
2026-03-25 18:46:30 +0200

    Added user requests for patch and http functions

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,7 +1,16 @@
 import { protectedApi } from './axiosConfig';
-import { type User } from '@models/userTypes';
+import { type User, type UserFields } from '@models/userTypes';
 
 export const createOrFetchUser = async (): Promise<User> => {
     const response = await protectedApi.post<User>('/user');
     return response.data;
 };
+
+export const updateUserFields = async (fieldsObj: UserFields): Promise<User> => {
+    const response= await protectedApi.patch('/user', fieldsObj)
+    return response.data
+}
+
+export const deleteUser = async () => {
+    await protectedApi.delete('/user')
+}
