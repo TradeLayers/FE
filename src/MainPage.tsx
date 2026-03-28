cfc441487d08119f6eeb4b73658955c48efd88c0
Raleese <titaslr38@gmail.com>
2026-03-28 18:13:06 +0200

    Added Added About Us and Learn navigation buttons in header

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,5 +1,5 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import { Box, Typography } from '@mui/material';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Box, Button, Typography } from '@mui/material';
 import { signOut } from 'firebase/auth';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -8,15 +8,16 @@ import { UserType } from '@models/userTypes';
 import { type RootState } from '@store/store';
 import { resetUserInfo } from '@store/userSlice';
 import LogInButton from './components/LogInButton';
-import { HeaderRow, PageWrapper } from './MainPage.styles';
+import { HeaderActions, HeaderRow, NavButton, PageWrapper } from './MainPage.styles';
 
 const MainPage: React.FC = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     const dispatch = useDispatch();
     const userType = useSelector((state: RootState) => state.userSliceName.userType);
     const isLoggedIn = userType !== UserType.Guest;
 
-    const handleAuthButtonClick = async () => {
+    const handleAuthButtonClick = async (): Promise<void> => {
         if (!isLoggedIn) {
             navigate('/login');
             return;
@@ -27,13 +28,37 @@ const MainPage: React.FC = () => {
         navigate('/');
     };
 
+    const handleNavigate = (path: string): void => {
+        navigate(path);
+    };
+
+    const isActivePath = (path: string): boolean => location.pathname === path;
+
     return (
         <Box sx={PageWrapper}>
             <Box sx={HeaderRow}>
                 <Typography variant="h4" component="h1">
                     Stock Tracker
                 </Typography>
-                <LogInButton isLoggedIn={isLoggedIn} onClick={handleAuthButtonClick} />
+                <Box sx={HeaderActions}>
+                    <Button
+                        variant={isActivePath('/about') ? 'contained' : 'outlined'}
+                        size="large"
+                        sx={NavButton}
+                        onClick={() => handleNavigate('/about')}
+                    >
+                        About Us
+                    </Button>
+                    <Button
+                        variant={isActivePath('/learn') ? 'contained' : 'outlined'}
+                        size="large"
+                        sx={NavButton}
+                        onClick={() => handleNavigate('/learn')}
+                    >
+                        Learn
+                    </Button>
+                    <LogInButton isLoggedIn={isLoggedIn} onClick={handleAuthButtonClick} />
+                </Box>
             </Box>
             <Outlet />
         </Box>
