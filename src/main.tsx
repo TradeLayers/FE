1ae6d5e11f834b7bafd914a9e5df2efd5efa2ca9
Titas <titaslr38@gmail.com>
2026-03-29 21:26:06 +0300

    Added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { Provider } from 'react-redux';
 import store from '@store/store.ts';
+import { isGuest } from '@models/userTypes';
+import { type RootState } from '@store/store';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -18,6 +21,17 @@ const LoginPage = lazy(() => import('./features/Login/LoginPage.tsx'));
 const HomePage = lazy(() => import('./features/Home/HomePage.tsx'));
 const AboutPage = lazy(() => import('./features/About/AboutPage.tsx'));
 const LearnPage = lazy(() => import('./features/Learn/LearnPage.tsx'));
+const AccountPage = lazy(() => import('./features/Account/AccountPage.tsx'));
+
+const RequireUser: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const isLoggedIn = useSelector((state: RootState) => !isGuest(state.userSliceName));
+
+    if (!isLoggedIn) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return <>{children}</>;
+};
 
 function Main(): React.JSX.Element {
     return (
@@ -27,6 +41,14 @@ function Main(): React.JSX.Element {
                     <Route index element={<HomePage />} />
                     <Route path="about" element={<AboutPage />} />
                     <Route path="learn" element={<LearnPage />} />
+                    <Route
+                        path="account"
+                        element={
+                            <RequireUser>
+                                <AccountPage />
+                            </RequireUser>
+                        }
+                    />
                     <Route path="*" element={<HomePage />} />
                 </Route>
                 <Route path="login" element={<LoginPage />} />
