49b93ba54660159a133586416d23a1b559a08b5f
Steponas Kaminskas <132608893+SteponasK@users.noreply.github.com>
2026-03-29 23:02:25 +0300

    Add stock models

--- src/models/stockTypes.ts
+++ src/models/stockTypes.ts
@@ -2,3 +2,26 @@ export type Stock = {
     stockName: string;
     symbol: string;
 };
+
+export type StockListItem = {
+    symbol: string;
+    name: string;
+    price: number;
+};
+
+export type StockSearchResult = {
+    symbol: string;
+    description: string;
+    type: string;
+};
+
+export type StockProfile = {
+    symbol: string;
+    name: string;
+    logo: string;
+    industry: string;
+    country: string;
+    exchange: string;
+    marketCap: number;
+    webUrl: string;
+};
