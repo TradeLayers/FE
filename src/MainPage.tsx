144f2c92db33ae15ebf38dc26f129e1d58858a65
Titas <titaslr38@gmail.com>
2026-03-29 21:27:25 +0300

    Added routing

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -4,6 +4,7 @@ import { signOut } from 'firebase/auth';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { auth } from '@configs/firebase';
+import { isGuest } from '@models/userTypes';
 import { type RootState } from '@store/store';
 import { resetUserInfo } from '@store/userSlice';
 import { addInfo } from '@store/informationSplice';
@@ -11,8 +12,6 @@ import { InfoMessageStatus, type Information } from '@models/informationType';
 import { InformationDisplay } from './features/informationDisplay/infoSnackBar';
 import LogInButton from './components/LogInButton';
 import { HeaderActions, HeaderRow, NavButton, PageWrapper } from './MainPage.styles';
-
-const isGuest = (user: { name?: string }): boolean => user.name === 'Guest';
 
 const MainPage: React.FC = () => {
     const navigate = useNavigate();
@@ -85,6 +84,16 @@ const MainPage: React.FC = () => {
                     >
                         Learn
                     </Button>
+                    {isLoggedIn && (
+                        <Button
+                            variant={isActivePath('/account') ? 'contained' : 'outlined'}
+                            size="large"
+                            sx={NavButton}
+                            onClick={() => handleNavigate('/account')}
+                        >
+                            Account
+                        </Button>
+                    )}
                     <LogInButton isLoggedIn={isLoggedIn} onClick={handleAuthButtonClick} />
                 </Box>
             </Box>
